3874b846333d1950ddf3abbb8ffd7ace39e2a14c
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-02-24 02:33:03 +0700

    update

--- web-Blog-MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace web_Blog_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
