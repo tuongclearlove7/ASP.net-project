421c8a98bb3961ff946ee4bd506a9d885ff5f7cc
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-03-03 02:56:00 +0700

    update

---
+++ web-profile-MVC-VS2019/web-profile-MVC-VS2019/Models/documentation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_profile_MVC_VS2019.Models
+{
+    public class documentation
+    {
+
+        public string image { get; set; }
+        public string text { get; set; }
+
+
+        public documentation() { }
+    }
+}
