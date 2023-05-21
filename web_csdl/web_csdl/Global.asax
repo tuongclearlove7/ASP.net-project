6213fff88112d70474a4d5a4ec41158282258597
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-05-21 22:42:00 +0700

    update source code

---
+++ web_csdl/web_csdl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_csdl.Global" Language="C#" %>
---
+++ web_csdl/web_csdl/Models/dbo.sanpham.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[sanpham] ([masp], [tensp], [dongia], [soluong], [mota], [hinh_anh]) VALUES (1234, N'ao', 10000, 1, N'aosss', N'image')
