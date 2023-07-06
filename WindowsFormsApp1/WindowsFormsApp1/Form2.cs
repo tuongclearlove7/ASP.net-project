3e5a20344c32fb66f177d4f091d628c63f7c3636
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-07-06 18:01:53 +0700

    update source

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        int i = 0;
+        public Form2()
+        {
+            InitializeComponent();
+            //label1.Text = "Count : " + i;
+            //i++;
+        }
+
+      
+    }
+}
